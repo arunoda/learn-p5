b59b41a54f781188d2c7f646c53cdf6a6adfa9c6
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2025-11-28 13:23:23 +0530

    Add mouse support

---
+++ 05-mouse/sketch.js
@@ -0,0 +1,23 @@
+let diamteter = 50;
+
+function setup() {
+    // Runs once
+    createCanvas(600, 400); // width, height
+}
+
+function draw() {
+    // Runs every frame (default ~60 fps)
+    background(30);
+
+    // circle
+    noStroke();
+    fill(200);
+    circle(mouseX, mouseY, diamteter);
+}
+
+function mousePressed() {
+    diamteter += 20;
+    if (diamteter > 200) {
+        diamteter = 50;
+    }
+}

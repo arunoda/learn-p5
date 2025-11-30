ae8ad46ec707d3db39ca07a7c6b4497292123208
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2025-11-30 06:02:34 +0530

    Add 3d bouncing ball

---
+++ nature-of-code/02.2-bouncing-ball-3d/sketch.js
@@ -0,0 +1,47 @@
+let position;
+let velocity;
+
+let sphereRadius = 20;
+let boxSize = 100;
+
+function setup() {
+    // Runs once
+    createCanvas(600, 400, WEBGL); // width, height
+
+    position = createVector(0, 0, 0);
+    velocity = createVector(1, 1, 2);
+}
+
+function draw() {
+    // Runs every frame (default ~60 fps)
+    background(30);
+
+    orbitControl();
+
+    position.add(velocity);
+
+    if (abs(position.x) > boxSize/2) {
+        velocity.x *= -1;
+    }
+
+    if (abs(position.y) > boxSize/2) {
+        velocity.y *= -1;
+    }
+
+    if (abs(position.z) > boxSize/2) {
+        velocity.z *= -1;
+    }
+
+    push();
+    fill(255);
+    stroke(200)
+    translate(position.x, position.y, position.z);
+    sphere(sphereRadius, 20)
+    pop();
+
+    push();
+    noFill();
+    stroke(255);
+    box(boxSize + sphereRadius * 2);
+    pop();
+}
